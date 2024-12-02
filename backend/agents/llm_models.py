38b39bec9ffb3a1b5d35d627ac151c7c61a61f83
Minki Jung <qmsoqm2@gmail.com>
2024-12-02 06:21:39 -0500

    add groq and cerebras inference APIs

--- backend/agents/llm_models.py
+++ backend/agents/llm_models.py
@@ -1,21 +1,23 @@
 import os
 from dotenv import load_dotenv
-
-from langchain_core.runnables import ConfigurableField
-from langchain_openai import ChatOpenAI, OpenAI
-from langchain_anthropic import ChatAnthropic, Anthropic
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+from langchain_cerebras import ChatCerebras
+from langchain_groq import ChatGroq
 
 load_dotenv('.env', override=True)
-DEFAULT_MODEL = os.getenv("DEFAULT_MODEL", "claude-3-5-sonnet-latest")
+DEFAULT_MODEL = os.getenv("DEFAULT_MODEL", "gpt-4o")
 FALLBACK_MODEL = os.getenv("FALLBACK_MODEL", "gpt-4o-2024-08-06")
 LLM_TEMPERATURE = float(os.getenv("LLM_TEMPERATURE", 0.7))
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+CEREBRAS_API_KEY = os.getenv("CEREBRAS_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 if "claude" in DEFAULT_MODEL:
     if not ANTHROPIC_API_KEY:
         raise ValueError("Anthropic API key is not set")
-    
+
     chat_model = ChatAnthropic(
         model=DEFAULT_MODEL,
         api_key=ANTHROPIC_API_KEY,
@@ -45,7 +47,7 @@ if "claude" in DEFAULT_MODEL:
 elif "gpt" in DEFAULT_MODEL:
     if not OPENAI_API_KEY:
         raise ValueError("OpenAI API key is not set")
-    
+
     chat_model = ChatOpenAI(
         model=DEFAULT_MODEL,
         api_key=OPENAI_API_KEY,
@@ -68,6 +70,41 @@ elif "gpt" in DEFAULT_MODEL:
             ChatAnthropic(
                 model=FALLBACK_MODEL,
                 api_key=ANTHROPIC_API_KEY,
+                temperature=LLM_TEMPERATURE,
+            )
+        ]
+    )
+elif "cerebras" in DEFAULT_MODEL:
+    if not CEREBRAS_API_KEY:
+        raise ValueError("Cerebras API key is not set")
+
+    chat_model = ChatCerebras(
+        model="llama3.1-70b",
+        api_key=CEREBRAS_API_KEY,
+        temperature=LLM_TEMPERATURE,
+    ).with_fallbacks(
+        [
+            ChatOpenAI(
+                model=FALLBACK_MODEL,
+                api_key=OPENAI_API_KEY,
+                temperature=LLM_TEMPERATURE,
+            )
+        ]
+    )
+elif "groq" in DEFAULT_MODEL:
+    if not GROQ_API_KEY:
+        raise ValueError("Groq API key is not set")
+
+    chat_model = ChatGroq(
+        model="llama3-groq-70b-8192-tool-use-preview",
+        # model="llama-3.1-8b-instant",
+        api_key=GROQ_API_KEY,
+        temperature=LLM_TEMPERATURE,
+    ).with_fallbacks(
+        [
+            ChatOpenAI(
+                model=FALLBACK_MODEL,
+                api_key=OPENAI_API_KEY,
                 temperature=LLM_TEMPERATURE,
             )
         ]
